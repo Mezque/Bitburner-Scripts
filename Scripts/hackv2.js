5d707d1008b2241bab02cdaebd7fdbfbe1b6e6ab
Mezque <31026406+Mezque@users.noreply.github.com>
2022-01-23 16:37:49 -0400

    formate

--- Scripts/hackv2.js
+++ Scripts/hackv2.js
@@ -14,21 +14,30 @@
     var maxMoney = ns.getServerMaxMoney(target);
     ns.tprint("and with a max money count of ", maxMoney);
 
-    while (true) {
-      if (ns.getServerSecurityLevel(target) > securityThresh) {
+    while (true)
+    {
+      if (ns.getServerSecurityLevel(target) > securityThresh)
+      {
         await ns.weaken(target);
-        if (ns.args[3] == true) {
+        if (ns.args[3] == true)
+        {
           ns.tprint("Weaken Server on ", (target));
         }
-      } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      }
+      else if (ns.getServerMoneyAvailable(target) < moneyThresh)
+      {
         await ns.grow(target);
-        if (ns.args[3] == true) {
+        if (ns.args[3] == true)
+        {
           ns.tprint("Grow Server on ", (target), " Server needs ", ns.getServerMaxMoney(target) * ns.args[1] - ns.getServerMoneyAvailable(target), " more money before first hack is possible");
         }
-      } else {
+      }
+      else
+      {
         await ns.hack(target);
-        if (ns.args[3] == true) {
-        ns.tprint("Hack Server ran on ", (target), " Server needs ", ns.getServerMaxMoney(target) * ns.args[1] - ns.getServerMoneyAvailable(target), " more money before next hack is possible");
+        if (ns.args[3] == true)
+        {
+            ns.tprint("Hack Server ran on ", (target), " Server needs ", ns.getServerMaxMoney(target) * ns.args[1] - ns.getServerMoneyAvailable(target), " more money before next hack is possible");
         }
       }
     }
